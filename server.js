89a909965d70dfc1b5e8c03611d75ce389a28a6d
ajitkumarpal1 <66736656+ajitkumarpal1@users.noreply.github.com>
2024-05-18 00:18:42 +0530

    remove env

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import server from "./index.js"
 
-server.listen(process.env.PORT, () => {
+/* server.listen(process.env.PORT, () => {
     console.log(`server is runig on port ${process.env.PORT}`)
 })
+ */
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
